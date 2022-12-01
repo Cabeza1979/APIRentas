df0d917ccaa7432b175131a647e40100a72b7d98
Christian Tronconi <chtronconi@gmail.com>
2022-12-01 14:33:11 -0300

    para deploy

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ server.get("/",(req, res)=>{
     res.status(200).json(response);
 
    } catch (error) {
-    return res.status(400).json(error);
+    return res.status(404).json(error);
    }
 });
 
