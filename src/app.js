db5974aee5f2b9e1f007f500f34ab62b6d9ec104
Christian Tronconi <chtronconi@gmail.com>
2022-12-01 12:26:02 -0300

    para deploy

--- src/app.js
+++ src/app.js
@@ -17,5 +17,21 @@ server.get("/", (req,res)=>{
        }
 });
 
+server.put("/",(req, res)=>{
+    const {codigocaso, statuscode, EsFinalizado, Cuit} = req.body;
+   try {
+    if (!codigocaso || !statuscode || !EsFinalizado || !Cuit) throw Error("missing info");
+    const response = {
+        CodigoError: "UCE-01",
+        Descripcion: "Estado actualizado correctamente",
+        Fecha: "2022-11-02T12:45:30.764Z"
+    };
+    res.status(200).json(response);
+
+   } catch (error) {
+    return res.status(400).json(error);
+   }
+});
+
 
 module.exports = server;
