b8e88eeb39a8f860339d64af823fb0df9dc1c25b
Cabeza1979 <chtronconi@gmail.com>
2023-11-24 14:31:54 -0300

    se invierte cuentas par/impar

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ server.post("/rentas",(req, res)=>{
     var condicion="NP";
     
     try {
-       if(cuenta_in%2){
+       if(!cuenta_in%2){
             condicion="P";
             fechaVigenciaDesde="01/01/2025";
        }
